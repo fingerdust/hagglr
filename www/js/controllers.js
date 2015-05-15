98a12634311a2fc97f7c07f7a2dd932c0e375b2a
Mark Tobin <mtspark@gmail.com>
2015-05-15 13:29:12 +0100

    controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,10 +12,63 @@ app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 }); 
 
-app.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+app.controller('AccountCtrl', function($scope, $http, $location) {
+    $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+ 
+    $scope.login = function() {
+        
+        var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/userinfo.email&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
+        ref.addEventListener('loadstart', function(event) { 
+            if((event.url).startsWith("http://localhost/callback")) {
+                requestToken = (event.url).split("code=")[1];
+                $http({method: "post", url: "https://accounts.google.com/o/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
+                    .success(function(data) {
+                        accessToken = data.access_token;
+                        initUserData(accessToken); 
+                        $location.path("/account");
+                        
+                    })
+                    .error(function(data, status) {
+                        alert("ERROR: " + data);
+                    });
+                ref.close();
+            }
+        });
+    }
+ 
+    if (typeof String.prototype.startsWith != 'function') {
+        String.prototype.startsWith = function (str){
+            return this.indexOf(str) == 0;
+        };
+    }
+
+    var initUserData = function(accessToken){
+    	$http.get("https://www.googleapis.com/userinfo/email?alt=json&access_token=" + accessToken)
+    		.success(function(data){
+    			USER_EMAIL = data.data.email; 
+                alert(USER_EMAIL); 
+    		})
+    		.error(function(data, status){
+    			alert("ERROR:" + data); 
+    		}); 
+    }
+    /*
+    var adduser = function(){
+        $http({
+            method: "post",
+            url: "http://127.0.0.1/adduser.php", 
+            data: {
+                email: USER_EMAIL
+            }
+        })
+        .success(function(data) {
+            alert(data); 
+        })
+        .error(function(data){
+            alert(data)
+        }); 
+    }
+    */
 });
 
 app.controller('ScannerController', function($scope, $cordovaBarcodeScanner, BarcodeService, $state){
@@ -29,14 +82,6 @@ app.controller('ScannerController', function($scope, $cordovaBarcodeScanner, Bar
 	}; 
 }); 
 
-app.service("BarcodeService", function(){
-	this.setBarcode = function(barcode){
-		this.barcode = barcode; 
-	}; 
-	this.getBarcode = function(){
-		return this.barcode; 
-	}; 
-}); 
 
 app.controller("ProductController", function($scope, $cordovaBarcodeScanner, $http, BarcodeService){
 	var barcode = BarcodeService.getBarcode(); 
@@ -44,7 +89,7 @@ app.controller("ProductController", function($scope, $cordovaBarcodeScanner, $ht
 		method: "post",
 		url: "http://ec2-52-11-37-35.us-west-2.compute.amazonaws.com/Logic/amazon/index.php", 
 		data: {
-			number: "050644651557"
+			number: barcode
 		},
 		headers: { 
 			'Content-Type': 'application/x-www-form-urlencoded'
